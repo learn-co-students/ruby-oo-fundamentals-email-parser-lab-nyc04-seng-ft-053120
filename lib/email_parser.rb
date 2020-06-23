a92b430fc27ff846387d2135c3526ed1865aac64
kceron <karemiceron@gmail.com>
2020-06-22 23:40:41 -0400

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -16,7 +16,7 @@ class EmailAddressParser
     end 
 end 
 
-email_addresses = "john@doe.com, person@somewhere.org"
-parser = EmailAddressParser.new(email_addresses)
+#email_addresses = "john@doe.com, person@somewhere.org"
+#parser = EmailAddressParser.new(email_addresses)
 
-parser.parse 
+#parser.parse 
